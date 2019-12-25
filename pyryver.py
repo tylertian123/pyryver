da08bd4361c54a06c73f0620748c3dedc00b5851
Tyler Tian <tylertian123@gmail.com>
2019-12-25 15:34:00 -0500

    Add get_cached_list and get_obj_by_field
    Add get_cached_list, which gets the list of all users/forums/teams from
    a JSON file if it exists, or gets it from Ryver if it does not.
    
    Add get_obj_by_field and a bunch of fields, which simply gets the
    user/forum/team with a field matching a value.

--- pyryver.py
+++ pyryver.py
@@ -1,6 +1,8 @@
 import requests
 from base64 import b64encode
 from getpass import getpass
+import os
+import json
 
 class Credentials:
     def __init__(self, org=None, user=None, password=None):
@@ -21,11 +23,36 @@ TYPE_USERS = "users"
 TYPE_FORUMS = "forums"
 TYPE_TEAMS = "workrooms"
 
+FIELD_USER_USERNAME = "username"
+FIELD_USER_EMAIL_ADDR = "emailAddress"
+FIELD_USER_DISPLAY_NAME = "displayName"
+
+FIELD_NAME = "name"
+FIELD_NICKNAME = "nickname"
+FIELD_ID = "id"
+
 def get_list(cred, objtype):
     url = cred.url_prefix + objtype
     resp = requests.get(url, headers=cred.headers)
     resp.raise_for_status()
     return resp.json()["d"]["results"]
+
+def get_cached_list(cred, objtype, name=None):
+    name = name or "pyryver." + objtype + ".json"
+    if os.path.exists(name):
+        with open(name, "r") as f:
+            return json.load(f)
+    else:
+        data = get_list(cred, objtype)
+        with open(name, "w") as f:
+            json.dump(data, f)
+        return data
+
+def get_obj_by_field(objs, field, value):
+    for obj in objs:
+        if obj[field] == value:
+            return obj
+    return None
 
 def get_topics(cred, objtype, objid, archived=False):
     url = cred.url_prefix + f"{objtype}({objid})/Post.Stream(archived={'true' if archived else 'false'})?$format=json"
