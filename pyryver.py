2fd851792e2917e7cbd2f1394dd4cc951099c076
Tyler Tian <tylertian123@gmail.com>
2019-12-27 00:54:40 -0500

    Add return type declarations and format
    Add return type declarations on some functions that are missing them and
    format with autopep8.

--- pyryver.py
+++ pyryver.py
@@ -9,6 +9,7 @@ import typing
 import os
 import json
 
+
 class Creator:
     """
     A message creator, with a name and an avatar.
@@ -19,7 +20,7 @@ class Creator:
     def __init__(self, name: str, avatar: str):
         self.name = name
         self.avatar = avatar
-    
+
     def to_dict(self) -> dict:
         """
         Convert this Creator object to a dictionary to be used in a request.
@@ -31,17 +32,19 @@ class Creator:
             "avatar": self.avatar
         }
 
+
 class Object:
     """
     Base class for all Ryver objects.
     """
+
     def __init__(self, cred, obj_type: str, data: dict):
         self.cred = cred
         self.data = data
         self.obj_type = obj_type
         self.entity_type = ENTITY_TYPES[obj_type]
         self.id = data["id"]
-    
+
     def get_id(self) -> typing.Any:
         """
         Get the ID of this object.
@@ -49,13 +52,13 @@ class Object:
         This is usually an integer, however for messages it is a string instead.
         """
         return self.id
-    
+
     def get_type(self) -> str:
         """
         Get the type of this object.
         """
         return self.obj_type
-    
+
     def get_raw_data(self) -> dict:
         """
         Get the raw data of this object.
@@ -65,10 +68,12 @@ class Object:
         """
         return self.data
 
+
 class TopicReply(Object):
     """
     A reply on a topic.
     """
+
     def get_message(self) -> str:
         """
         Get the message of this reply.
@@ -81,7 +86,7 @@ class TopicReply(Object):
         """
         return self.data["createUser"]["id"]
 
-    def get_author(self):
+    def get_author(self) -> "User":
         """
         Get the author of this reply, as a User object.
 
@@ -89,17 +94,19 @@ class TopicReply(Object):
         """
         return self.cred.get_object(TYPE_USER, self.data["createUser"]["id"])
 
+
 class Topic(Object):
     """
     A Ryver topic in a chat.
     """
     # TODO: Add function to get replies
+
     def get_subject(self) -> str:
         """
         Get the subject of this topic.
         """
         return self.data["subject"]
-    
+
     def get_body(self) -> str:
         """
         Get the body of this topic.
@@ -125,16 +132,18 @@ class Topic(Object):
         resp.raise_for_status()
         return TopicReply(self.cred, TYPE_TOPIC_REPLY, resp.json()["d"]["results"])
 
+
 class Message(Object):
     """
     A Ryver chat message.
     """
+
     def get_body(self) -> str:
         """
         Get the message body.
         """
         return self.data["body"]
-    
+
     def get_creator(self) -> Creator:
         """
         Get the Creator of this message.
@@ -147,14 +156,14 @@ class Message(Object):
             return Creator(self.data["createSource"]["displayName"], self.data["createSource"]["avatar"])
         else:
             return None
-    
+
     def get_author_id(self) -> int:
         """
         Get the ID of the author of this message.
         """
         self.data["from"]["id"]
 
-    def get_author(self):
+    def get_author(self) -> "User":
         """
         Get the author of this message, as a User object.
 
@@ -162,7 +171,7 @@ class Message(Object):
         """
         return self.cred.get_object(TYPE_USER, self.data["from"]["id"])
 
-    def get_chat_type(self):
+    def get_chat_type(self) -> str:
         """
         Gets the type of chat that this message was sent to, as a string.
 
@@ -181,7 +190,7 @@ class Message(Object):
         """
         return self.data["to"]["id"]
 
-    def get_chat(self):
+    def get_chat(self) -> "Chat":
         """
         Get the chat that this message was sent to, as a Chat object.
 
@@ -189,12 +198,13 @@ class Message(Object):
         """
         return self.cred.get_object(get_type_from_entity(self.get_chat_type()), self.get_chat_id())
 
-    def react(self, emoji: str):
+    def react(self, emoji: str) -> None:
         """
         React to a message with an emoji, specified with the string name (e.g. "thumbsup").
         """
         url = self.cred.url_prefix + \
-            "{chat_type}({chat_id})/Chat.React()".format(chat_type=self.get_chat_type(),chat_id=self.get_chat_id())
+            "{chat_type}({chat_id})/Chat.React()".format(
+                chat_type=self.get_chat_type(), chat_id=self.get_chat_id())
         data = {
             "id": self.id,
             "reaction": emoji
@@ -210,15 +220,17 @@ class Message(Object):
         Returns a dict of {emoji: number_of_reacts}
         """
         reactions = self.data['__reactions']
-        counts = {reaction: len(users) for reaction, users in reactions.items()}
+        counts = {reaction: len(users)
+                  for reaction, users in reactions.items()}
         return counts
 
-    def delete(self):
+    def delete(self) -> None:
         """
         Deletes the message.
         """
         url = self.cred.url_prefix + \
-            "{chat_type}({chat_id})/Chat.DeleteMessage()?%24format=json".format(chat_type=self.get_chat_type(),chat_id=self.get_chat_id())
+            "{chat_type}({chat_id})/Chat.DeleteMessage()?%24format=json".format(
+                chat_type=self.get_chat_type(), chat_id=self.get_chat_id())
         data = {
             "id": self.id,
         }
@@ -226,10 +238,12 @@ class Message(Object):
         resp = requests.post(url, json=data, headers=self.cred.headers)
         resp.raise_for_status()
 
+
 class Chat(Object):
     """
     A Ryver chat (forum, team, user, etc).
     """
+
     def send_message(self, message: str, creator: Creator = None) -> str:
         """
         Send a message to this chat.
@@ -239,7 +253,8 @@ class Chat(Object):
         Returns the ID of the chat message sent. Note that message IDs are
         strings.
         """
-        url = self.cred.url_prefix + f"{self.obj_type}({self.id})/Chat.PostMessage()"
+        url = self.cred.url_prefix + \
+            f"{self.obj_type}({self.id})/Chat.PostMessage()"
         data = {
             "body": message
         }
@@ -248,7 +263,7 @@ class Chat(Object):
         resp = requests.post(url, json=data, headers=self.cred.headers)
         resp.raise_for_status()
         return resp.json()["d"]["id"]
-    
+
     def create_topic(self, subject: str, body: str, creator: Creator = None) -> Topic:
         """
         Create a topic in this chat.
@@ -277,7 +292,7 @@ class Chat(Object):
         resp = requests.post(url, json=data, headers=self.cred.headers)
         resp.raise_for_status()
         return Topic(self.cred, TYPE_TOPIC, resp.json()["d"]["results"])
-    
+
     def get_topics(self, archived: bool = False, top: int = -1, skip: int = 0) -> typing.List[Topic]:
         """
         Get all the topics in this chat.
@@ -287,32 +302,37 @@ class Chat(Object):
 
         Note that this method does send requests, so it may take some time.
         """
-        url = self.cred.url_prefix + f"{self.obj_type}({self.id})/Post.Stream(archived={'true' if archived else 'false'})?$format=json"
-        topics = get_all(url, self.cred.headers, param="&$skip", top=top, skip=skip)
+        url = self.cred.url_prefix + \
+            f"{self.obj_type}({self.id})/Post.Stream(archived={'true' if archived else 'false'})?$format=json"
+        topics = get_all(url, self.cred.headers,
+                         param="&$skip", top=top, skip=skip)
         return [Topic(self.cred, TYPE_TOPIC, data) for data in topics]
-    
+
     def get_messages(self, count: int) -> typing.List[Message]:
         """
         Get a number of messages (most recent first) in this chat.
 
         Note that this method does send requests, so it may take some time.
         """
-        url = self.cred.url_prefix + f"{self.obj_type}({self.id})/Chat.History()?$format=json&$top={count}"
-        resp = requests.get(url, headers = self.cred.headers)
+        url = self.cred.url_prefix + \
+            f"{self.obj_type}({self.id})/Chat.History()?$format=json&$top={count}"
+        resp = requests.get(url, headers=self.cred.headers)
         resp.raise_for_status()
         messages = resp.json()["d"]["results"]
         return [Message(self.cred, TYPE_MESSAGE, data) for data in messages]
+
 
 class User(Chat):
     """
     A Ryver user.
     """
+
     def get_username(self) -> str:
         """
         Get the username of this user.
         """
         return self.data["username"]
-    
+
     def get_display_name(self) -> str:
         """
         Get the display name of this user.
@@ -325,41 +345,47 @@ class User(Chat):
 
         Note that this method does send requests, so it may take some time.
         """
-        url = self.cred.url_prefix + f"{self.obj_type}({self.id})/User.Active.Set(value='{'true' if activated else 'false'}')"
+        url = self.cred.url_prefix + \
+            f"{self.obj_type}({self.id})/User.Active.Set(value='{'true' if activated else 'false'}')"
         resp = requests.post(url, headers=self.cred.headers)
         resp.raise_for_status()
+
 
 class Forum(Chat):
     """
     A Ryver forum.
     """
+
     def get_name(self) -> str:
         """
         Get the name of this forum.
         """
         return self.data["name"]
-    
+
     def get_nickname(self) -> str:
         """
         Get the nickname of this forum.
         """
         return self.data["nickname"]
 
+
 class Team(Chat):
     """
     A Ryver team.
     """
+
     def get_name(self) -> str:
         """
         Get the name of this team.
         """
         return self.data["name"]
-    
+
     def get_nickname(self) -> str:
         """
         Get the nickname of this team.
         """
         return self.data["nickname"]
+
 
 class Ryver:
     """
@@ -371,6 +397,7 @@ class Ryver:
     input(). If the password is not provided, it will be prompted using 
     getpass().
     """
+
     def __init__(self, org: str = None, user: str = None, password: str = None):
         if not org:
             org = input("Organization: ")
@@ -384,7 +411,7 @@ class Ryver:
             "authorization": "Basic " + b64encode((user + ":" + password).encode("ascii")).decode("ascii")
         }
         self.url_prefix = "https://" + org + ".ryver.com/api/1/odata.svc/"
-    
+
     def get_object(self, obj_type: str, obj_id: int) -> Object:
         """
         Get an object from Ryver with a type and ID.
@@ -392,7 +419,7 @@ class Ryver:
         Note that this method does send requests, so it may take some time.
         """
         url = self.url_prefix + f"{obj_type}({obj_id})"
-        resp = requests.get(url, headers = self.headers)
+        resp = requests.get(url, headers=self.headers)
         resp.raise_for_status()
         return TYPES_DICT[obj_type](self, obj_type, resp.json()["d"]["results"])
 
@@ -409,7 +436,7 @@ class Ryver:
         url = self.url_prefix + obj_type
         chats = get_all(url, self.headers, top=top, skip=skip)
         return [TYPES_DICT[obj_type](self, obj_type, chat) for chat in chats]
-    
+
     def get_cached_chats(self, obj_type: str, force_update: bool = False, name: str = None, top: int = -1, skip: int = 0) -> typing.List[Chat]:
         """
         Attempt to load a list of chats (teams, forums, users, etc) from JSON,
@@ -433,6 +460,7 @@ class Ryver:
             with open(name, "w") as f:
                 json.dump([chat.data for chat in chats], f)
             return chats
+
 
 TYPE_USER = "users"
 TYPE_FORUM = "forums"
@@ -471,6 +499,7 @@ FIELD_NAME = "name"
 FIELD_NICKNAME = "nickname"
 FIELD_ID = "id"
 
+
 def get_obj_by_field(objs: typing.List[Object], field: str, value: typing.Any) -> Object:
     """
     Gets an object from a list of objects by a field.
@@ -483,7 +512,8 @@ def get_obj_by_field(objs: typing.List[Object], field: str, value: typing.Any) -
             return obj
     return None
 
-def get_all(url: str, headers: dict, top: int = -1, skip: int = 0, param: str = "?$skip"):
+
+def get_all(url: str, headers: dict, top: int = -1, skip: int = 0, param: str = "?$skip") -> typing.List[dict]:
     """
     Because the REST API only gives 50 results at a time, this function is used
     to retrieve all objects.
@@ -499,7 +529,8 @@ def get_all(url: str, headers: dict, top: int = -1, skip: int = 0, param: str = 
         count = min(top, 50)
         top -= count
 
-        resp = requests.get(url + f"{param}={skip}&$top={count}", headers=headers)
+        resp = requests.get(
+            url + f"{param}={skip}&$top={count}", headers=headers)
         resp.raise_for_status()
         page = resp.json()["d"]["results"]
         result.extend(page)
@@ -507,6 +538,7 @@ def get_all(url: str, headers: dict, top: int = -1, skip: int = 0, param: str = 
             break
         skip += len(page)
     return result
+
 
 def get_type_from_entity(entity_type: str) -> str:
     """
