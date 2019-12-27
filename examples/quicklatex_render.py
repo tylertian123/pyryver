c2a377877046a83a2bed6eed879b954769f1c1b3
Moeez Muhammad <mmoeez48@gmail.com>
2019-12-26 22:20:04 -0500

    Add naming to formatted string of command
    Now, instead of having a {0}, it has a {formula} for better readability

--- examples/quicklatex_render.py
+++ examples/quicklatex_render.py
@@ -1,7 +1,7 @@
 import subprocess
 
-cmd = "curl 'https://quicklatex.com/latex3.f' -H 'Content-Type: application/x-www-form-urlencoded' --data 'formula={0}&fsize=60px&fcolor=a9a9a9&mode=0&out=1&remhost=quicklatex.com&preamble=\\usepackage{{amsmath}}\n\\usepackage{{amsfonts}}\n\\usepackage{{amssymb}}'"
+cmd = "curl 'https://quicklatex.com/latex3.f' -H 'Content-Type: application/x-www-form-urlencoded' --data 'formula={formula}&fsize=60px&fcolor=a9a9a9&mode=0&out=1&remhost=quicklatex.com&preamble=\\usepackage{{amsmath}}\n\\usepackage{{amsfonts}}\n\\usepackage{{amssymb}}'"
 
 def ql_render(formula: str) -> str:
-    p = subprocess.Popen(cmd.format(formula.replace("&", "%26")), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(cmd.format(formula=formula.replace("&", "%26")), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return p.stdout.read().decode("ascii").split()[1]
